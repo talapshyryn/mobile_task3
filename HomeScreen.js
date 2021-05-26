ec6946a3ae9dbc66f25728c4861a0b939116f101
talapshyryn <talapova05@gmail.com>
2021-05-26 20:32:18 +0600

    initial commit

---
+++ HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { FriendsContext } from './FriendsContext';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>You have { this.context.currentFriends.length } friends.</Text>
+
+        <Button
+          title="Add some friends"
+          onPress={() =>
+            this.props.navigation.navigate('Friends')
+          }
+        />
+      </View>
+    );
+  }
+}
+HomeScreen.contextType = FriendsContext;
+// ...
